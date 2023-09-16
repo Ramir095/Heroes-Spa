941b6b5201cdfa90be3007b527fc32a03b9c165d
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-09-15 21:51:50 -0300

    chore: commented lines

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -4,18 +4,20 @@ import { Navigate, useLocation } from 'react-router-dom';
 
 export const PrivateRoute = ({ children }) => {
 
-  const { authState } = useContext(AuthContext);
+  // const { authState } = useContext(AuthContext);
+  const { logged } = useContext(AuthContext);
   const { pathname, search } = useLocation();
 
   const lastPath = pathname + search; // La ruta donde se encuentra el usuario + las query que busca el usuario
   
   useEffect(() => {
-    localStorage.setItem('lastPath', lastPath)
+    localStorage.setItem('lastPath', lastPath) // Guardamos la ruta en el local Storage
   }, [lastPath])
 
   return (
-    (authState.logged) // Si es true
-    ? children // devolveme las rutas
+     // (authState.logged) Si es true
+    (logged)
+    ? children // devolveme las rutas privadas
     : <Navigate to='/login' />
   )
 }
